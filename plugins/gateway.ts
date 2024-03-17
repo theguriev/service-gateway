a8cfc1282de99b34e501413616840eb969f74086
Eugen Guriev <gurievcreative@gmail.com>
2024-03-17 18:38:54 +0200

    refactor: route handling in gateway plugin

--- plugins/gateway.ts
+++ plugins/gateway.ts
@@ -3,17 +3,25 @@ import { safeDestr } from 'destr'
 import type { NitroRouteConfig } from 'nitropack'
 import type { RouterMethod } from 'h3'
 
+type Route = NitroRouteConfig & { methods: Array<RouterMethod>; authorizationNeeded?: boolean; };
+
 export default defineNitroPlugin((app) => {
   try {
     const { routesFile, secret } = useRuntimeConfig()
     const file = readFileSync(routesFile, 'utf-8')
-    const routes = safeDestr<{ [path: string]: NitroRouteConfig & { method: RouterMethod; authorizationNeeded?: boolean; } }>(file)
+    const routes = safeDestr<{ [path: string]: Route }>(file)
 
-    Object.entries(routes).forEach(([path, route]) => {
-      const method = (route.method || 'get').toLowerCase() as RouterMethod
-      if (method in app.router) {
+    Object.entries(routes).forEach(([path, { methods, authorizationNeeded, proxy }]) => {
+      if (!Array.isArray(methods)) {
+        return
+      }
+      methods.forEach((unpreparedMethod) => {
+        const method = unpreparedMethod.toLowerCase() as RouterMethod
+        if (!(method in app.router)) {
+          return
+        }
         app.router.add(path, defineEventHandler(async (event) => {
-          if (route.authorizationNeeded) {
+          if (authorizationNeeded) {
             const accessToken = getCookie(event, 'accessToken')
             if (!accessToken) {
               throw createError({ message: 'Access token not found!', status: 404 })
@@ -26,17 +34,17 @@ export default defineNitroPlugin((app) => {
             }
           }
 
-          if (route.proxy) {
-            if (typeof route.proxy === 'string') {
-              return proxyRequest(event, route.proxy)
+          if (proxy) {
+            if (typeof proxy === 'string') {
+              return proxyRequest(event, proxy)
             }
-            if ('to' in route.proxy) {
-              return proxyRequest(event, route.proxy.to, route.proxy)
+            if ('to' in proxy) {
+              return proxyRequest(event, proxy.to, proxy)
             }
           }
           return new Response('No proxy defined', { status: 500 })
         }), method)
-      }
+      })
     })
   } catch (error) {
     console.error('Initialize error: ', error)
